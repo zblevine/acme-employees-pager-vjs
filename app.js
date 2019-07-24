4c44a89a35798712981ba638ea466bfa15bbe0b1
Stella Kim <testcodesk@gmail.com>
2019-07-23 20:50:59 -0400

    2nd

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const loadData = async () => {
-    const response = await fetch('https://acme-users-api-rev.herokuapp.com/api/users/2');
+const url = 'https://acme-users-api-rev.herokuapp.com/api/users/'
+
+const loadData = async (page) => {
+    const response = await fetch(`${url}${page}`);
     const data = await response.json();
     const users = data.users;
-    console.log(data);
-    const firstUser = users[27];
-    console.log(`User 0: ${firstUser.firstName} ${firstUser.lastName}, ${firstUser.title}, ${firstUser.email}`)
+    const tbody = document.querySelector('#table_body')
+    // console.log(`${userLastName} ${userFirstName}, ${userEmail }, ${userTitle}`)
+    html = '';
+    users.forEach(user => {
+        html += `<tr><td>${user.firstName}</td><td>${user.lastName}</td><td>${user.email}</td>
+        <td>${user.title}</td></tr>`
+    });
+
+    tbody.innerHTML = html;
+
+
 }
 
-loadData();
+let currentPageNo = 0;
+loadData(currentPageNo);
+
+// cosnt firstPage = 
